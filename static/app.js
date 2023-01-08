547f2f9831ba1bb1d256153022bf50e40e6ec0a2
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-01-08 08:53:46 +0530

    fixed the javascript issues inside django app

--- static/app.js
+++ static/app.js
@@ -1,13 +1,4 @@
-
-
-
-let navbar = document.querySelector('.navigation-tablets-mobile');
 let dis = 0;
-let  header = document.querySelector('.header');
-let sections = document.querySelectorAll('section:not(.navigation-tablets-mobile');
-
-const nav = this.document.querySelector('.nav');
-
 
 // functions to show log-in and signup part when user clicks on it.
 function func1(){
@@ -36,42 +27,40 @@ function loginClose(){
 
 // Fuctions to display navigation-links on mobile and tablet;
 
-// document.querySelector('.navbar').addEventListener('click', showNav);
+
+function showNav(){
+    console.log(dis)
+    document.querySelector('.navigation-tablets-mobile').style.display='flex';
+    dis = 1;
+    document.body.style.opacity='.8';
+    document.body.style.overflow='hidden';
+    sections = document.querySelectorAll('section:not(.navigation-tablets-mobile');
+    // console.log(sections);
+    sections.forEach(section => {
+        section.addEventListener('click', hideNav);
+    });
+}
 
 
-// function showNav(){
-//     console.log(dis)
-//     document.querySelector('.navigation-tablets-mobile').style.display='flex';
-//     dis = 1;
-//     document.body.style.opacity='.8';
-//     document.body.style.overflow='hidden';
-// }
 
-
-// sections = document.querySelectorAll('section:not(.navigation-tablets-mobile');
-// console.log(sections);
-// sections.forEach(section => {
-//     section.addEventListener('click', hideNav);
-// });
- 
-
-// function hideNav(){
-//     if(dis == 1){
-//         console.log(dis);
-//         document.querySelector('.navigation-tablets-mobile').style.display="none";
-//         document.body.style.opacity='1';
-//         document.body.style.overflow='initial';
-//         dis = 0;
-//     }
-// }
+function hideNav(){
+    if(dis == 1){
+        console.log(dis);
+        document.querySelector('.navigation-tablets-mobile').style.display="none";
+        document.body.style.opacity='1';
+        document.body.style.overflow='initial';
+        dis = 0;
+    }
+}
 
 // adding background color to navbar while scrolling.
 
 window.addEventListener("scroll", function(){
     let scrHeight = window.scrollY;
     let header = document.querySelector('.header')
+    // console.log(header);
 
-    console.log(scrHeight);
+    // console.log(scrHeight);
     
     if (scrHeight > 200){
         header.style.background="rgb(41,41,41)";
