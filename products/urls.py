03e6e54c15afa09d073d05abf91b5f76416b6f3f
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-01-09 08:12:56 +0530

    added user registration for ecommerce app

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')  # root url
-] 
+    path('', views.index, name='index1'),   # root url
+    path('register', views.register, name='register')
+]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,30 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+import time
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index1.html')
+
+
+def register(request):
+    if (request.method == 'POST'):
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if (User.objects.filter(email=email).exists()):
+            messages.info(request, "email already register , try logging in")
+            time.sleep(10)
+            return render(request, 'index1.html')
+        else:
+            user = User.objects.create_user(
+                username=firstname, email=email, password=password)
+
+            user.save()
+            return render(request, 'index1.html')
